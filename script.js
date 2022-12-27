a6052a5d589e5589088568c43e390f31df37152f
Fanta10 <fantakamate107@gmail.com>
2022-12-27 18:49:19 +0000

    commit version 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+fruits.sort();
+console.log(fruits);
+fruits.push("Kiwi");
+console.log(fruits);
+fruits.splice(0 , 1);
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
